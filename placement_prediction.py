862d8421c32a141825b8379b03c4fff8c2e3be77
Alvyn Abranches <alvynabranches@gmail.com>
2021-01-29 15:20:14 +0530

    Updated placement_prediction.py

--- placement_prediction.py
+++ placement_prediction.py
@@ -4,15 +4,16 @@ import pandas as pd
 import pickle as pkl
 from time import perf_counter
 import matplotlib.pyplot as plt
+from scipy.sparse.construct import random
 import seaborn as sns
-from sklearn.model_selection import StratifiedKFold, cross_val_score
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 from utils import preprocess_data_placement
 
@@ -58,6 +59,17 @@ df.loc[m, 'HSC_Marks'] = df.loc[m, 'HSC_Marks'].fillna(df['HSC_Marks'])
     'Semester7_Marks', 'BackPapers7', 'P_BackPapers7', 'HSC_Marks', 'SSC_Marks', 'Diploma_Marks', 'dead_back_log', 'live_atkt', 
     'Job Offer Count'
 ]
-campus_encoder = LabelEncoder()
-df['Campus'] = campus_encoder.fit_transform()
+
+branch_encoder, campus_encoder, gender_encoder = LabelEncoder(), LabelEncoder(), LabelEncoder()
+pkl.dump(df['Campus'], open(f'{unique_dir}/Campus.sav','wb'))
+df['Campus'] = campus_encoder.fit_transform(df['BRANCH'])
 pkl.dump(campus_encoder, open(f'{encoder_dir}/Campus.sav','wb'))
+df['BRANCH'] = branch_encoder.fit_transform(df['BRANCH'])
+pkl.dump(df['Campus'], open(f'{unique_dir}/Gender.sav','wb'))
+df['Gender'] = gender_encoder.fit_transform(df['Gender'])
+pkl.dump(campus_encoder, open(f'{encoder_dir}/Gender.sav','wb'))
+
+X = df.iloc[:,df.columns!='Job Offer Count']
+y = df['Job Offer Count']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=2019)
